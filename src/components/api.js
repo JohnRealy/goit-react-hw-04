3bea43a802ee588be35c8d2e2abe6b5595443f84
JohnRealy <branderok@gmail.com>
2025-02-01 21:59:43 +0100

    Баг з деплойом

--- src/components/api.js
+++ src/components/api.js
@@ -2,6 +2,12 @@ import axios from "axios";
 
 const API_KEY = import.meta.env.VITE_UNSPLASH_KEY;
 
+if (!API_KEY) {
+  console.error(
+    "API ключ не знайдено! Переконайтесь, що він доданий у Vercel."
+  );
+}
+
 export default function api({ searchQuery, page }) {
   axios.defaults.baseURL = "https://api.unsplash.com/";
   axios.defaults.headers.common["Authorization"] = `Client-ID ${API_KEY}`;
