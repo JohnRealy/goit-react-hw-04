130ac47f847b284d4f2428f9927c6df102bc9745
JohnRealy <branderok@gmail.com>
2025-02-01 22:05:42 +0100

    Update api.js

--- src/components/api.js
+++ src/components/api.js
@@ -3,9 +3,7 @@ import axios from "axios";
 const API_KEY = import.meta.env.VITE_UNSPLASH_KEY;
 
 if (!API_KEY) {
-  console.error(
-    "API ключ не знайдено! Переконайтесь, що він доданий у Vercel."
-  );
+  console.error("API ключ не знайдено!");
 }
 
 export default function api({ searchQuery, page }) {
